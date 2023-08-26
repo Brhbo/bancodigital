51f85c03c029dd4f79aaa8cbcfa0b58d4cba1f63
Brhbo <brunohenriqueborges@hotmail.com>
2023-08-26 12:17:54 +0100

    Initial commit

---
+++ src/Banco.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Rafaela Reis";
+        String tipoConta = "Corrente";
+        double saldo = 1599.99;
+
+        System.out.println("*************************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("tipo conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("\n*************************");
+    }
+}
